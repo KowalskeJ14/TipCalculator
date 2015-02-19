8e46e2ff4d038783b6c06c4d9d0014b8b6bd36e3
JKowalske2 <JKowalske2@L11712042775.wctc.msft>
2015-02-19 15:12:47 -0600

    Changed some of the names of the Classes to have better naming conventions.

---
+++ src/PercentageTipStrategy.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JKowalske2
+ */
+public class PercentageTipStrategy implements TipCalculator{
+
+    private double billAmount;
+    private double percentage;
+    
+    public PercentageTipStrategy(double amount, double percent){
+        setBillAmount(amount);
+        setPercentage(percent);
+    }
+
+    public double getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+    
+    @Override
+    public double getTip() {
+        return billAmount * (percentage / 100);
+    }
+    
+}
